b3d4d88fc3b84266733ef76c76653e601c92ad49
BoppreH <boppreh@gmail.com>
2013-09-06 21:02:04 -0300

    Add Unit tests

--- test_units.py
+++ test_units.py
@@ -2,21 +2,37 @@ import unittest
 from units import Unit
 
 class TestUnit(unittest.TestCase):
+    def setUp(self):
+        self.empty = Unit()
+        self.num = Unit(('a',), ())
+        self.den = Unit((), ('b',))
+        self.full = Unit(('a',), ('b',))
+        self.multiple = Unit(('a', 'a', 'c'), ('b', 'd'))
+
     def test_creation(self):
-        u = Unit()
-        self.assertFalse(u)
-        self.assertEquals(u.numerator, [])
-        self.assertEquals(u.denominator, [])
+        self.assertFalse(self.empty)
+        self.assertEquals(self.empty.numerator, [])
+        self.assertEquals(self.empty.denominator, [])
 
-        u = Unit(('a',))
-        self.assertTrue(u)
-        self.assertEquals(u.numerator, ['a'])
-        self.assertEquals(u.denominator, [])
+        self.assertTrue(self.num)
+        self.assertEquals(self.num.numerator, ['a'])
+        self.assertEquals(self.num.denominator, [])
 
-        u = Unit((), ('a',))
-        self.assertTrue(u)
-        self.assertEquals(u.numerator, [])
-        self.assertEquals(u.denominator, ['a'])
+        self.assertTrue(self.den)
+        self.assertEquals(self.den.numerator, [])
+        self.assertEquals(self.den.denominator, ['b'])
+
+        self.assertTrue(self.multiple)
+        self.assertEquals(self.multiple.numerator, ['a', 'a', 'c'])
+        self.assertEquals(self.multiple.denominator, ['b', 'd'])
+
+    def test_mul(self):
+        self.assertEquals(self.multiple * self.full, self.full * self.multiple)
+        self.assertEquals(self.empty * self.num, self.num)
+        self.assertEquals(self.empty * self.den, self.den)
+        self.assertEquals(self.num * self.den, self.full)
+
+
 
 if __name__ == '__main__':
     unittest.main()
